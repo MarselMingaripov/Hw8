3b0c96866fcc20a7bd5140b2a208e7f98ecd94f3
MarselMingaripov <bard1812@gmail.com>
2022-12-12 15:27:50 +0300

    выполнил задание1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+//        task2();
+//        task3();
+//        task4();
+    }
+
+    public static void task1(){
+        int[] array1 = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        boolean[] array3 = new boolean[2];
+        array3[0] = true;
+        array3[1] = false;
     }
 }
