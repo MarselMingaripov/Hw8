f300aa9d351fe4144079fc890ea26cf96d630d3e
MarselMingaripov <bard1812@gmail.com>
2022-12-12 15:36:14 +0300

    выполнил задание2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-//        task2();
+        task2();
 //        task3();
 //        task4();
     }
@@ -18,4 +18,35 @@ public class Main {
         array3[0] = true;
         array3[1] = false;
     }
+
+    public static void task2(){
+        System.out.println("task2");
+        int[] array1 = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        boolean[] array3 = new boolean[2];
+        array3[0] = true;
+        array3[1] = false;
+
+        for (int i = 0; i <array1.length; i++){
+            System.out.print(array1[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i <array2.length; i++){
+            System.out.print(array2[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i <array3.length; i++){
+            System.out.print(array3[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
 }
