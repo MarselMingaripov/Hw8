184ce2e4623096e2b264d3e23866e9834db9db3f
MarselMingaripov <bard1812@gmail.com>
2022-12-12 15:51:16 +0300

    исправил задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-//        task3();
+        task3();
 //        task4();
     }
 
@@ -52,6 +52,48 @@ public class Main {
 
         for (int i = 0; i <array3.length; i++){
             if (i == array3.length - 1){
+                System.out.print(array3[i]);
+                break;
+            }
+            System.out.print(array3[i] + ", ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task3(){
+        System.out.println("task3");
+        int[] array1 = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        boolean[] array3 = new boolean[2];
+        array3[0] = true;
+        array3[1] = false;
+
+        for (int i = array1.length - 1; i >= 0; i--){
+            if (i == 0){
+                System.out.print(array1[i]);
+                break;
+            }
+            System.out.print(array1[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = array2.length - 1; i >= 0; i--){
+            if (i == 0){
+                System.out.print(array2[i]);
+                break;
+            }
+            System.out.print(array2[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = array3.length - 1; i >= 0; i--){
+            if (i == 0){
                 System.out.print(array3[i]);
                 break;
             }
